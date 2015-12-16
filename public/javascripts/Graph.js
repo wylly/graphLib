488738762e932b5962ba9786416b8c2be0b4c8c1
Wylly <wylly31@wp.pl>
2015-12-16 10:49:58 +0100

    2nd commit

---
+++ public/javascripts/Graph.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Wylly on 2015-12-15.
+ */
+var Graph = new function(){
+    this.nodesList = [];
+    this.transitionsList = [];
+
+
+    this.init = function(){
+        //INITIALIZE
+    }
+
+    this.saveGraph = function(){
+
+    }
+}
---
+++ public/javascripts/Node.js
@@ -0,0 +1,10 @@
+var Node = new function (){
+    this.xCoord = 0;
+    this.yCoord = 0;
+    this.transitionsList = [];
+
+
+    this.getNeihgbours = function(){
+
+    }
+}
